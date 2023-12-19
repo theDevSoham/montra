69828a3cabbf07b73c824822516af5098ad924a8
Soham Das <publicsoham.24@gmail.com>
2023-12-19 22:44:56 +0530

    adding important libraries

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+import AppNavigator from './app/navigators/AppNavigator';
 
 const App = () => {
   useEffect(() => {
@@ -12,9 +14,20 @@ const App = () => {
       clearTimeout(timer);
     };
   }, []);
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato',
+      secondary: 'yellow',
+    },
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text>App</Text>
+      <PaperProvider theme={theme}>
+        <AppNavigator />
+      </PaperProvider>
     </SafeAreaView>
   );
 };
@@ -24,5 +37,6 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'red',
   },
 });
---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
